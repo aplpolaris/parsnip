4ee5fb04ae00c055867b3a0f82605ed7811b330b
Elisha Peterson <elisha.peterson@jhuapl.edu>
2025-04-22 16:39:36 -0400

    2025 copyrights

--- parsnip/src/test/kotlin/edu/jhuapl/data/parsnip/datum/transform/FieldEncodeTest.kt
+++ parsnip/src/test/kotlin/edu/jhuapl/data/parsnip/datum/transform/FieldEncodeTest.kt
@@ -6,7 +6,7 @@ package edu.jhuapl.data.parsnip.datum.transform
  * FieldEncodeTest.kt
  * edu.jhuapl.data:parsnip
  * %%
- * Copyright (C) 2019 - 2024 Johns Hopkins University Applied Physics Laboratory
+ * Copyright (C) 2019 - 2025 Johns Hopkins University Applied Physics Laboratory
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
